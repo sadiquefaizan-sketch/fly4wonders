a32111ac2ee38777ac71358c68e91cb76ba869f4
Sadiq Faizan <sadiquefaizan@gmail.com>
2026-06-24 05:50:29 +0530

    Fix AuthModal formatting

--- src/App.jsx
+++ src/App.jsx
@@ -162,7 +162,8 @@ const packages = [
     </header>
   );
 };
-   const AuthModal = () => (  <div style={{
+   const AuthModal = () => (
+  <div style={{
       position: 'fixed', top: 0, left: 0, right: 0, bottom: 0,
       background: 'rgba(0,0,0,0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 1000
     }}>
